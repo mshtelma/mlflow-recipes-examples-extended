b33dda563239af343c9342a6c83a0242eb76f692
Michael Shtelma <michael.shtelma@databricks.com>
2022-12-21 23:35:05 +0100

    Initial Commit

--- text-classification-tfidf/tests/train_test.py
+++ text-classification-tfidf/tests/train_test.py
@@ -10,7 +10,7 @@ def test_tranform_fn_returns_object_with_correct_spec():
     # pylint: enable=assignment-from-none
     if estimator:
         df = pd.read_parquet(
-            "./text-classification-tfidf/data/complaints_cicd.parquet"
+            "./text-classification-tfidf/data/complaints_small.parquet"
         )[:100]
         transformed_df = transformer.fit_transform(df)
         estimator.fit(transformed_df, df["category_id"])
