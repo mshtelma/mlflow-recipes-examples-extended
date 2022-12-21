515a97f6c8a27b89579669baac17efe396847839
Michael Shtelma <michael.shtelma@databricks.com>
2022-12-21 23:27:30 +0100

    Initial Commit

---
+++ text-classification-tfidf/steps/transform.py
@@ -0,0 +1,56 @@
+"""
+This module defines the following routines used by the 'transform' step of the regression recipe:
+
+- ``transformer_fn``: Defines customizable logic for transforming input data before it is passed
+  to the estimator during model inference.
+"""
+from sklearn.compose import ColumnTransformer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import (
+    TfidfTransformer,
+    CountVectorizer,
+)
+from sklearn.pipeline import Pipeline
+
+
+def transformer_fn():
+    """
+    Returns an *unfitted* transformer that defines ``fit()`` and ``transform()`` methods.
+    The transformer's input and output signatures should be compatible with scikit-learn
+    transformers.
+    """
+    pipeline = Pipeline(
+        [
+            (
+                "ct",
+                ColumnTransformer(
+                    [
+                        (
+                            "Consumer_complaint_narrative",
+                            Pipeline(
+                                [
+                                    (
+                                        "count",
+                                        CountVectorizer(
+                                            stop_words="english", max_features=5000
+                                        ),
+                                    ),
+                                    (
+                                        "tfid",
+                                        TfidfTransformer(
+                                            sublinear_tf=True,
+                                            norm="l2",
+                                        ),
+                                    ),
+                                    ("svd", TruncatedSVD(n_components=250)),
+                                ]
+                            ),
+                            0,
+                        )
+                    ]
+                ),
+            )
+        ],
+        verbose=True,
+    )
+    return pipeline
---
+++ text-classification-transformers/tests/train_test.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from steps.transform import transformer_fn
+from steps.train import estimator_fn
+
+
+def test_tranform_fn_returns_object_with_correct_spec():
+    # pylint: disable=assignment-from-none
+    transformer = transformer_fn()
+    estimator = estimator_fn()
+    # pylint: enable=assignment-from-none
+    if estimator:
+        df = pd.read_parquet("./text-classification-transformers/data/complaints_cicd.parquet")
+        transformed_df = transformer.fit_transform(df)
+        estimator.fit(transformed_df, df["category_id"])
+        pred_df = estimator.predict(transformed_df)
+        assert len(df) == len(pred_df)
